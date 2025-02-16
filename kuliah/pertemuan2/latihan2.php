0701f5deefc89633707ca4ffd9ad8352a4eaf9a1
Phatuurr <muhamad.fatur.rahaman@gmail.com>
2025-02-16 16:21:51 +0700

    menyelesainkai tugas php

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+    $nama = "Muhammad Fatur Rahman";
+    $email = "phatooor@gmail.com";
+    $pekerjaan = "Graphic Designer";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universita Pasundan";
+
+
+    echo "<h1> $nama </h1>";
+    echo "<p>$email</p>";
+    echo "Halo nama Saya $nama, saya adalah seorang $pekerjaan, saya adalah seorang mahasiswa $universitas jurusan $jurusan.";
+?>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $npm = 94;
+        echo "<b>$npm</b></br>";
+
+        $hasilkali = $npm*5;
+        echo "<b>$hasilkali</b></br>";
+
+        $hasilbagi = $hasilkali/2;
+        echo "<b>$hasilbagi</b></br>";
+
+        $hasiltambah = $hasilbagi+75;
+        echo "<b>$hasiltambah</b></br>";
+
+        $hasilkurang = $hasiltambah-20;
+        echo "<b>$hasilkurang</b></br>";
+    ?>
+</body>
+</html>

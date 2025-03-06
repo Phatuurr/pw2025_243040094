e308333fdcda6aa6e6f8472b3dfbd999ed8531ae
Phatuurr <muhamad.fatur.rahaman@gmail.com>
2025-03-06 15:00:03 +0700

    menyelesaikan tugas 3

--- tugas/tugas3/3a.php
+++ tugas/tugas3/3a.php
@@ -6,7 +6,7 @@ function hitungluaslingkaran($r) {
 }
 $r = 10;
 echo "jari-jari = $r<br>";
-echo "Luas Linkaran = ";
+echo "Luas Lingkaran = ";
 echo hitungluaslingkaran(10);
 echo " cm²";
 echo "<hr>";
@@ -19,7 +19,7 @@ echo "<hr>";
 
 $r = 20;
 echo "jari-jari = $r<br>";
-echo "keliling Linkaran = ";
+echo "keliling Lingkaran = ";
 echo hitungkelilinglingkaran(20);
 echo " cm";
 echo "<hr>";
